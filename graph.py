9e7e29410d4252cd69f0b3f41867dd6289e70cd6
robaldis <robertaldis@gmail.com>
2021-10-04 17:12:47 +0100

    feat: binary to array implementation

--- graph.py
+++ graph.py
@@ -44,16 +44,45 @@ class Graph():
 
         return bit_string 
 
-    def binary_to_array(self):
-        pass
+    def binary_to_array(self, bstring):
+        length = bstring[:24].uint
+        barray = bstring[24:].bin
+        array = list()
 
+        print(length)
+
+        start = 0
+        for i in range(1, length):
+            string_array = list(barray[start:start+i])
+            numbers = [int(x) for x in string_array]
+            array.append(numbers)
+            start += i
+
+        return array
 
 def main():
-    graph = Graph(size=1000)
+    
+    graph = Graph(size=10000)
+
+    # Test the time it takes to convert to binary
     start = time.time()
-    graph.array_to_binary()
+    bstring = graph.array_to_binary()
     print(time.time() - start)
+
+    # Test the time it takes to convert binary to an array
+    start = time.time()
+    array = graph.binary_to_array(bstring)
+    print(time.time() - start)
+
+    print("-"*20)
+    # Make sure they are the same array
+    print(graph.array == array)
    
+    """
+    For 10,000 nodes in the graph:
+    array_to_binary time: 15.594651460647583
+    binary_to_array tim:  9.157088279724121
+    """
 
 if __name__ == "__main__":
     main()
